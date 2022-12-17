0d61294ee0e04d55db57cbc22f9359634d98aff0
rishi-kumar <rishikumar10dec@gmail.com>
2022-12-17 10:48:18 +0530

    added function that operate on two numbers

--- app.js
+++ app.js
@@ -15,3 +15,25 @@ function multiply(a, b){
 function divide(a, b){
     return (a / b).toFixed(3);
 }
+
+
+function operate(operator, a, b){
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a, b);
+    
+        case '-':
+            return subtract(a, b);
+
+        case '/':
+            return divide(a, b);
+
+        case '*':
+            return multiply(a, b);
+
+        default:
+            break;
+    }
+}
