a5db6d36725ca7edf4a7012e65bbb5cddd4cfb65
rishi-kumar <rishikumar10dec@gmail.com>
2022-12-17 10:34:47 +0530

    defined basic math functions

---
+++ app.js
@@ -0,0 +1,17 @@
+// functions for basic mathmatical operations
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return (a / b).toFixed(3);
+}
